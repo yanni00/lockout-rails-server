160f36ed195a1a1c8d4e36ce626f7e4a85d82c59
YANNI <yanniflambas@hotmail.com>
2020-02-27 18:46:00 +1100

    additions from class

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,13 @@ puts "Created #{ User.count } users."
 
 Venue.destroy_all
 
-v1 = Venue.create title: "Venue Test 1", address: "1 Market St Sydney NSW 2000",
-  visited: "Yanni"
+v1 = Venue.create title: "Venue Test 1", address: "1 Market St Sydney NSW 2000",  visited: "Yanni"
 v2 = Venue.create title: "Venue Test 2", address: "50 Market St Sydney NSW 2000",   visited: "Yanni"
 v3 = Venue.create title: "Venue Test 3", address: "Elizabeth St Sydney",   visited: "Yanni"
-
 v4 = Venue.create title: "Venue Test 4", address: "Elizabeth St",   visited: "Yanni"
-
 
 puts "Destroying venue..."
 
 puts "Done!"
 
 puts "Destroying Events"
-
-Event.destroy_all
-
-e1 = Venue.create title: "Venue Test 1", address: "New Years Eve Party",
-  visited: "Yanni"
