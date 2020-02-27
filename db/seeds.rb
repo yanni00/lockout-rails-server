2f50d5d867bd218c297eef7170ea5394402a0cd1
YANNI <yanniflambas@hotmail.com>
2020-02-27 13:34:24 +1100

    added slidebar functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ u1 = User.create! name:'yanni', email: 'yanni@test.com', address: '1 Market Stre
 
 puts "Created #{ User.count } users."
 
-# Venue.destroy_all
+Venue.destroy_all
 
 v1 = Venue.create title: "Venue Test 1", address: "1 Market St Sydney NSW 2000",
   visited: "Yanni"
@@ -14,9 +14,6 @@ v2 = Venue.create title: "Venue Test 2", address: "50 Market St Sydney NSW 2000"
 v3 = Venue.create title: "Venue Test 3", address: "Elizabeth St Sydney",   visited: "Yanni"
 
 v4 = Venue.create title: "Venue Test 4", address: "Elizabeth St",   visited: "Yanni"
-
-
-
 
 
 puts "Destroying venue..."
